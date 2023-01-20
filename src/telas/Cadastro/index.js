3e1dc31945dee01b09e3039333942bd510ac6762
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-01-19 22:09:31 -0300

    Refatoração nas telas de Cadastro e Login

--- src/telas/Cadastro/index.js
+++ src/telas/Cadastro/index.js
@@ -1,39 +1,40 @@
-import { useState } from 'react';
-import { View } from 'react-native';
-import { Alerta } from '../../componentes/Alerta';
-import Botao from '../../componentes/Botao';
-import { EntradaTexto } from '../../componentes/EntradaTexto';
-import styles from './styles';
-import { cadastrar } from '../../servicos/requisicoesFirebase';
+import { useState } from "react";
+import { View } from "react-native";
+import { Alerta } from "../../componentes/Alerta";
+import Botao from "../../componentes/Botao";
+import { EntradaTexto } from "../../componentes/EntradaTexto";
+import styles from "./styles";
+import { cadastrar } from "../../servicos/requisicoesFirebase";
+import { alteraDados } from "../../utils/comum";
 
 export default function Cadastro({ navigation }) {
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-    const [confirmaSenha, setConfirmaSenha] = useState('');
-    const [statusError, setStatusError] = useState('');
-    const [messageError, setMessageError] = useState('');
+    const [dados, setDados] = useState({
+        email: "",
+        senha: "",
+        confirmaSenha: "",
+    });
+
+    const [statusError, setStatusError] = useState("");
+    const [messageError, setMessageError] = useState("");
 
     async function realizarCadastro() {
-        if (email == '') {
-            setMessageError('Preencha com seu e-mail');
-            setStatusError('email');
-        } else if (senha == '') {
-            setMessageError('Digite sua senha');
-            setStatusError('senha');
-        } else if (confirmaSenha == '') {
-            setMessageError('Confirme sua senha');
-            setStatusError('confirmaSenha');
-        } else if (confirmaSenha != senha) {
-            setMessageError('As senhas não conferem');
-            setStatusError('confirmaSenha');
+        if (dados.email == "") {
+            setMessageError("Preencha com seu e-mail");
+            setStatusError("email");
+        } else if (dados.senha == "") {
+            setMessageError("Digite sua senha");
+            setStatusError("senha");
+        } else if (dados.confirmaSenha == "") {
+            setMessageError("Confirme sua senha");
+            setStatusError("confirmaSenha");
+        } else if (dados.confirmaSenha != dados.senha) {
+            setMessageError("As senhas não conferem");
+            setStatusError("confirmaSenha");
         } else {
-            const resultado = await cadastrar(email, senha);
-            setStatusError('firebase');
-            if (resultado == 'Sucesso') {
-                setMessageError('Usuário criado com sucesso!');
-                setEmail('');
-                setSenha('');
-                setConfirmaSenha('');
+            const resultado = await cadastrar(dados.email, dados.senha);
+            setStatusError("firebase");
+            if (resultado == "Sucesso") {
+                setMessageError("Usuário criado com sucesso!");
             } else {
                 setMessageError(resultado);
             }
@@ -44,31 +45,37 @@ export default function Cadastro({ navigation }) {
         <View style={styles.container}>
             <EntradaTexto
                 label="E-mail"
-                value={email}
-                onChangeText={texto => setEmail(texto)}
-                error={statusError == 'email'}
+                value={dados.email}
+                onChangeText={(val) =>
+                    alteraDados("email", val, dados, setDados)
+                }
+                error={statusError == "email"}
                 messageError={messageError}
             />
             <EntradaTexto
                 label="Senha"
-                value={senha}
-                onChangeText={texto => setSenha(texto)}
+                value={dados.senha}
+                onChangeText={(val) =>
+                    alteraDados("senha", val, dados, setDados)
+                }
                 secureTextEntry
-                error={statusError == 'senha'}
+                error={statusError == "senha"}
                 messageError={messageError}
             />
             <EntradaTexto
                 label="Confirmar Senha"
-                value={confirmaSenha}
-                onChangeText={texto => setConfirmaSenha(texto)}
+                value={dados.confirmaSenha}
+                onChangeText={(val) =>
+                    alteraDados("confirmaSenha", val, dados, setDados)
+                }
                 secureTextEntry
-                error={statusError == 'confirmaSenha'}
+                error={statusError == "confirmaSenha"}
                 messageError={messageError}
             />
 
             <Alerta
                 mensagem={messageError}
-                error={statusError == 'firebase'}
+                error={statusError == "firebase"}
                 setError={setStatusError}
                 tempo={2500}
             />
--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -6,12 +6,16 @@ import { EntradaTexto } from "../../componentes/EntradaTexto";
 import { auth } from "../../config/firebase";
 import { logar } from "../../servicos/requisicoesFirebase";
 import styles from "./styles";
+import { alteraDados } from "../../utils/comum";
 
 import animacaoCarregando from "../../../assets/animacaoCarregando.gif";
 
 export default function Login({ navigation }) {
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
+    const [dados, setDados] = useState({
+        email: "",
+        senha: "",
+    });
+
     const [statusError, setStatusError] = useState("");
     const [messageError, setMessageError] = useState("");
     const [carregando, setCarregando] = useState(true);
@@ -27,14 +31,14 @@ export default function Login({ navigation }) {
     }, []);
 
     async function realizarLogin() {
-        if (email == "") {
+        if (dados.email == "") {
             setMessageError("O e-mail é obrigatório");
             setStatusError("email");
-        } else if (senha == "") {
+        } else if (dados.senha == "") {
             setMessageError("A senha é obrigatório");
             setStatusError("senha");
         } else {
-            const resultado = await logar(email, senha);
+            const resultado = await logar(dados.email, dados.senha);
             if (resultado == "Erro") {
                 setStatusError("firebase");
                 setMessageError("E-mail ou Senha não conferem");
@@ -56,15 +60,19 @@ export default function Login({ navigation }) {
         <View style={styles.container}>
             <EntradaTexto
                 label="E-mail"
-                value={email}
-                onChangeText={(texto) => setEmail(texto)}
+                value={dados.email}
+                onChangeText={(val) =>
+                    alteraDados("email", val, dados, setDados)
+                }
                 error={statusError == "email"}
                 messageError={messageError}
             />
             <EntradaTexto
                 label="Senha"
-                value={senha}
-                onChangeText={(texto) => setSenha(texto)}
+                value={dados.senha}
+                onChangeText={(val) =>
+                    alteraDados("senha", val, dados, setDados)
+                }
                 secureTextEntry
                 error={statusError == "senha"}
                 messageError={messageError}
